b2f2ec3ac5221995d82ad819a76e8091e7fe3df5
June Zhao <bz.bijun@gmail.com>
2025-02-25 16:39:39 -0800

    update project component

--- src/ui/Projects.js
+++ src/ui/Projects.js
@@ -19,7 +19,7 @@ const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transit
 
 // const projectTitle = title.split(" ").join("-").toLowerCase();
 
-const Project = ({ title }) => {
+const Project = ({ title, description, link, imageSrc }) => {
   const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
     bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
   const projectTitle = title.split(" ").join("-").toLowerCase();
@@ -28,12 +28,22 @@ const Project = ({ title }) => {
     <motion.div variants={projectVariant} className="relative">
       <div className={overlayStyles}>
         <p className="text-2xl font-playfair">{title}</p>
-        <p className="mt-7">
-          Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Nulla
-          porttitor accumsan tincidunt.
-        </p>
+        <p className="mt-7">{description}</p>
+        {link && (
+          <div className="inline-flex pt-2">
+            <a
+              href={link}
+              className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r"
+            >
+              Live Demo
+            </a>
+          </div>
+        )}
       </div>
-      <img src={`../assets/${projectTitle}.jpeg`} alt={projectTitle} />
+      <img
+        src={imageSrc || `../assets/${projectTitle}.jpeg`}
+        alt={projectTitle}
+      />
     </motion.div>
   );
 };
@@ -62,9 +72,8 @@ const Projects = () => {
           </div>
         </div>
         <p className="mt-10 mb-10">
-          Aliquam, amet dui feugiat facilisi dui. Aliquam aliquet integer ut
-          fames odio in at. At magna ornare dictum lectus. Purus massa morbi
-          purus nec eget eleifend ut elit.
+          Some projects are freelance, some are from work, and a few are for
+          hobby.
         </p>
       </motion.div>
 
@@ -85,61 +94,66 @@ const Projects = () => {
             BEAUTIFUL USER INTERFACES
           </div>
 
-          {/* <Project title="Project 1" /> */}
-          <motion.div variants={projectVariant} className="relative">
-            <div className={overlayStyles}>
-              <p className="text-2xl font-playfair">Food Oasis</p>
-              <p className="mt-7">
-                Food Oasis is a web application that provides information about
+          {/* project 1 */}
+          <Project
+            title="Food Oasis"
+            description="A web application that provides information about
                 food pantries and meal programs to food seekers in Los Angeles
-                County
-              </p>
-              <div className="inline-flex pt-2">
-                <a
-                  href="https://github.com/hackforla/food-oasis"
-                  className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l"
-                >
-                  Code
-                </a>
-                <a
-                  href="https://foodoasis.la/"
-                  className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r"
-                >
-                  Demo
-                </a>
-              </div>
-            </div>
-
-            <img
-              src="https://res.cloudinary.com/dhhiphscp/image/upload/b_rgb:FFFFFF/c_pad,w_400,h_400/v1706560821/portfolio/fola-profolio_fznjfp.png"
-              alt="food-oasis"
-            />
-          </motion.div>
-
-          <Project title="Project 2" />
+                County"
+            link="https://foodoasis.la/"
+            imageSrc="https://res.cloudinary.com/dhhiphscp/image/upload/t_fola 400x400/v1706560821/portfolio/fola-profolio_fznjfp.png"
+          />
+          {/* project 2 */}
+          <Project
+            title="Shuna's Succulent House"
+            description="A static webpage featuring smooth animations, developed with HTML, CSS, and JavaScript for local plant nursery."
+            link="https://shuna-succulenthouse.com/"
+            imageSrc="https://res.cloudinary.com/dhhiphscp/image/upload/t_fola 400x400/v1740520056/project-2_ei4rjr.jpg"
+          />
 
           {/* ROW 2 */}
-          <Project title="Project 3" />
-          <Project title="Project 4" />
-          <Project title="Project 5" />
-
+          {/* project 3 */}
+          <Project
+            title="MapMyWay"
+            description="Full stack web application to track your travels built with
+                Node, Express, MongoDB, React, MUI, FramerMotion and Mapbox"
+            link="https://beige-gazelle-gear.cyclic.app/"
+            imageSrc="https://res.cloudinary.com/dhhiphscp/image/upload/t_fola 400x400/v1707329983/portfolio/Screenshot_2024-02-07_at_10.19.33_AM_u5fjun.png"
+          />
+          {/* project 4 */}
+          <Project
+            title="SY America"
+            description="E-commerce Shopify store for local hardware company"
+            link="https://www.syamericacasterwheels.com/"
+            imageSrc="https://res.cloudinary.com/dhhiphscp/image/upload/t_fola 400x400/v1740524580/portfolio/Screenshot_2025-02-25_at_3.02.52_PM_kpnnsx.png"
+          />
+          {/* project 5 */}
+          <Project
+            title="LongRun4U Caster Wheels"
+            description="E-commerce Shopify store for local caster wheels company. Build
+                with Shopify liquid."
+            link="https://longrun4u.com/"
+            imageSrc="https://res.cloudinary.com/dhhiphscp/image/upload/t_fola 400x400/v1740524865/portfolio/Screenshot_2025-02-25_at_3.07.17_PM_upnp52.png"
+          />
           {/* ROW 3 */}
-          <Project title="Project 6" />
-          {/* <Project title="Project 7" /> */}
-          <motion.div variants={projectVariant} className="relative">
-            <div className={overlayStyles}>
-              <p className="text-2xl font-playfair">Movie App</p>
-              <p className="mt-7">
-                Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.
-                Nulla porttitor accumsan tincidunt.
-              </p>
-            </div>
+          {/* project 6 */}
+          <Project
+            title="CineSearcher"
+            description="            JavaScript-based movie search site using Node.js, Express,
+                React, SCSS, integrated with TMDB API, styled with Bootstrap"
+            link="https://relaxed-jackson-4661d8.netlify.app/"
+            imageSrc="https://res.cloudinary.com/dhhiphscp/image/upload/t_fola 400x400/v1670700132/portfolio/MovieApp_xvv46t.png"
+          />
+          {/* project 7 */}
+          <Project
+            title="Han's Personal Guided Travel"
+            description="Local LA travel agency. Build with React, styled with Bootstrap
+"
+            link=""
+            imageSrc="https://res.cloudinary.com/dhhiphscp/image/upload/t_fola 400x400/v1735719513/Screenshot_2025-01-01_at_00-17-44_%E6%82%A6%E6%AD%A5%E6%97%85%E8%A1%8C-%E4%B8%93%E4%B8%9A%E6%8F%90%E4%BE%9B%E6%B4%9B%E6%9D%89%E7%9F%B6%E6%97%85%E6%B8%B8_%E7%BE%8E%E8%A5%BF%E6%97%85%E6%B8%B8_%E6%B4%9B%E6%9D%89%E7%9F%B6%E7%A7%81%E4%BA%BA%E5%AE%9A%E5%88%B6%E6%97%85%E6%B8%B8_%E6%B4%9B%E6%9D%89%E7%9F%B6%E5%8C%85%E8%BD%A6%E8%87%AA%E7%94%B1%E8%A1%8C_%E6%B4%9B%E6%9D%89%E7%9F%B6%E6%97%85%E6%B8%B8%E5%8C%85%E8%BD%A6%E7%AD%89%E6%9C%8D%E5%8A%A1_igvckt.png"
+          />
+          {/* project 8 */}
 
-            <img
-              src="https://res.cloudinary.com/dhhiphscp/image/upload/c_fill,w_400,h_400/v1670700132/portfolio/MovieApp_xvv46t.png"
-              alt="movie app"
-            />
-          </motion.div>
           <div
             className="flex justify-center text-center items-center p-10 bg-blue
               max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
